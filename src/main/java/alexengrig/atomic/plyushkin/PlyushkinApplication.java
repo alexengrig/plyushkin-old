d8f9f7f3a0edcbde107b2dd45f407c773e39c1ac
Grig Alex <alexengrigdev@gmail.com>
2019-06-13 21:44:21 +0300

    Init project via Spring Initializr

---
+++ src/main/java/alexengrig/atomic/plyushkin/PlyushkinApplication.java
@@ -0,0 +1,13 @@
+package alexengrig.atomic.plyushkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlyushkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlyushkinApplication.class, args);
+    }
+
+}
